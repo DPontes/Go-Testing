699cec932741e173858acf41a5f57975d9807548
DPontes <dpontes@protonmail.com>
2021-05-07 12:49:40 +0200

    Moved utils/ inside src/api/

--- src/api/utils/sort/sort_test.go
+++ src/api/utils/sort/sort_test.go
@@ -4,7 +4,7 @@ import (
   "testing"
 )
 
-func TestBubbleSort(t *testing.T) {
+func TestBubbleSortOrderDesc(t *testing.T) {
   // Init
   elements := []int{9,7,5,3,1,2,4,6,8,0}
 
@@ -15,7 +15,6 @@ func TestBubbleSort(t *testing.T) {
   if elements[0] != 9 {
     t.Error( "First element should be 9" )
   }
-
   if elements[len(elements) - 1] != 0 {
     t.Error( "Last element should be 0" )
   }
