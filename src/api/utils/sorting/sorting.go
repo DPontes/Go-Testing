f0fbd9c69eb2b3004590fc8a56591c77cdd73bcf
DPontes <dpontes@protonmail.com>
2021-05-18 14:27:46 +0200

    Implementation of ExecuteBubbleSortWithTimeOut
        Function uses Golang coroutines to assert that the BubbleSort
        algorithm doesn't take more than 500ms in this case.

--- src/api/utils/sorting/sorting.go
+++ src/api/utils/sorting/sorting.go
@@ -7,7 +7,7 @@ import (
 func BubbleSort(elements []int) {
   keepWorking := true
   for keepWorking {
-    keepWorking = false
+   keepWorking = false
 
     for i := 0; i < len(elements) - 1; i++ {
       if elements[i] > elements[i+1] {
--- src/api/utils/sorting/sorting_test.go
+++ src/api/utils/sorting/sorting_test.go
@@ -3,9 +3,39 @@ package sorting
 // To install the "assert" library below, run:
 // `go get github.com/stretchr/testify/assert`
 import (
+  "time"
   "testing"
   "github.com/stretchr/testify/assert"
 )
+
+func ExecuteBubbleSortWithTimeout(elements []int, t *testing.T) {
+  timeoutChan := make(chan bool, 1)
+
+  /*
+    Defers the execution of a function until the surrounding function returns.
+    The deferred call's arguments are evaluated immediately, but the function
+    call is not executed until the surrounding function returns.
+  */
+  defer close(timeoutChan)
+
+  go func() {
+    BubbleSort(elements)
+    timeoutChan <- false
+  }()
+
+  go func() {
+    time.Sleep(500 * time.Millisecond)
+    timeoutChan <- true
+  }()
+
+  // waits until the first coroutine returns a timeoutChan
+  // If timeoutChan is "true" (returned by the time.Sleep coroutine)
+  // the assert below will fail and the output will be printed out
+  if <- timeoutChan {
+    assert.Fail(t, "BubbleSort took more than 500ms")
+    return
+  }
+}
 
 func TestBubbleSortOrderIncre(t *testing.T) {
   elements := GetElements(10)
@@ -15,7 +45,7 @@ func TestBubbleSortOrderIncre(t *testing.T) {
   assert.EqualValues(t, 9, elements[0], "First element should be 9")
   assert.EqualValues(t, 0, elements[len(elements) - 1], "Last element should be 0")
 
-  BubbleSort(elements)
+  ExecuteBubbleSortWithTimeout(elements, t)
 
   assert.NotNil(t, elements)
   assert.EqualValues(t, 10, len(elements))
