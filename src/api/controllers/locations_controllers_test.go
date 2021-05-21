997476c21b8263b2f1101587264395431af5ce9d
DPontes <dpontes@protonmail.com>
2021-05-21 14:32:13 +0200

    Correct iplementation of TestGetCountryNotFound

--- src/api/controllers/locations_controllers_test.go
+++ src/api/controllers/locations_controllers_test.go
@@ -3,10 +3,10 @@ package controllers
 import(
   "testing"
   "os"
+  "api/utils/errors"
   "net/http"
   "net/http/httptest"
   "encoding/json"
-  "api/domain/locations"
   "github.com/mercadolibre/golang-restclient/rest"
   "github.com/stretchr/testify/assert"
   "github.com/gin-gonic/gin"
@@ -29,15 +29,18 @@ func TestGetCountryNotFound(t *testing.T) {
   response := httptest.NewRecorder()
   c, _ := gin.CreateTestContext(response)
   c.Request, _ = http.NewRequest(http.MethodGet, "", nil)
+  c.Params = gin.Params {
+    {Key: "country_id", Value: "AR"},
+  }
 
   GetCountry(c)
 
-  assert.EqualValues(t, http.StatusOK, response.Code)
+  assert.EqualValues(t, http.StatusNotFound, response.Code)
 
-  var country locations.Country
-  err := json.Unmarshal(response.Body.Bytes(), &country)
+  var apiErr errors.ApiError
+  err := json.Unmarshal(response.Body.Bytes(), &apiErr)
   assert.Nil(t, err)
 
-  assert.EqualValues(t, "AR", country.Id)
-  assert.EqualValues(t, "Argentina", country.Name)
+  assert.EqualValues(t, http.StatusNotFound, apiErr.Status)
+  assert.EqualValues(t, "Country not found", apiErr.Message)
 }
