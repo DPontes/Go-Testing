e398de3acfc9ff9a75eafa95945ff4937db0e2b9
DPontes <dpontes@protonmail.com>
2021-05-25 11:32:57 +0200

    Implement TestGetCountryNoError
        Test implemented in the same style as TestGetCountryNotFound,
        but this time we are mocking a correct return of data from the
        GetCountry function. Here we expect a country.Id = AR and a
        country.Name = Argentina
    
        Verification:
          - To break the test, just change the mock
            (e.g.: Change "Argentina" to "Argentinah")

--- src/api/controllers/locations_controllers_test.go
+++ src/api/controllers/locations_controllers_test.go
@@ -30,7 +30,7 @@ func (* locationsServiceMock) GetCountry(countryId string) (*locations.Country, 
 }
 
 func TestGetCountryNotFound(t *testing.T) {
-  // Mock LocationsService
+  // Mock LocationsService methods
   getCountryFunc = func(countryId string) (*locations.Country, *errors.ApiError) {
     return nil, &errors.ApiError{Status: http.StatusNotFound, Message: "Country not found"}
   }
@@ -54,4 +54,31 @@ func TestGetCountryNotFound(t *testing.T) {
 
   assert.EqualValues(t, http.StatusNotFound, apiErr.Status)
   assert.EqualValues(t, "Country not found", apiErr.Message)
+}
+
+func TestGetCountryNoError(t *testing.T) {
+  // Mock LocationsService methods
+  getCountryFunc = func(countryId string) (*locations.Country, *errors.ApiError) {
+    return &locations.Country{Id: "AR", Name: "Argentina"}, nil
+  }
+
+  services.LocationsService = &locationsServiceMock{}
+
+  response := httptest.NewRecorder()
+  c, _ := gin.CreateTestContext(response)
+  c.Request, _ = http.NewRequest(http.MethodGet, "", nil)
+  c.Params = gin.Params {
+    {Key: "country_id", Value: "AR"},
+  }
+
+  GetCountry(c)
+
+  assert.EqualValues(t, http.StatusOK, response.Code)
+
+  var country locations.Country
+  err := json.Unmarshal(response.Body.Bytes(), &country)
+  assert.Nil(t, err)
+
+  assert.EqualValues(t, "AR", country.Id)
+  assert.EqualValues(t, "Argentina", country.Name)
 }
